75525e49a9d9b43c5466829309ee66b615e76228
Ihor Vorontsov <ihor_vorontsov@epam.com>
2022-04-06 19:46:44 +0200

    Finish clean up

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -7,26 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        //Getting first vector from the user and calculating its length
         System.out.println("Enter x, y and z (coordinates) of the first vector each one in new line: ");
-        float x = scanner.nextFloat();
-        float y = scanner.nextFloat();
-        float z = scanner.nextFloat();
+        double x = scanner.nextFloat();
+        double y = scanner.nextFloat();
+        double z = scanner.nextFloat();
 
         Vector vector1 = new Vector(x, y, z);
-
         System.out.println("Vector length is = " + vector1.length());
 
+
+        //Getting second vector from the user and calculating dot product
         System.out.println("Enter x, y and z (coordinates) of the second vector each one in new line: ");
         x = scanner.nextFloat();
         y = scanner.nextFloat();
         z = scanner.nextFloat();
 
         Vector vector2 = new Vector(x, y, z);
-
         System.out.println("Result of 1st and 2nd vectors multiplication is = " + vector1.multBy(vector2));
 
-        System.out.println("Enter size (number of elements) of the array of random vectors");
 
+        //Generating array of random vectors
+        System.out.println("Enter size (number of elements) of the array of random vectors");
         int j = scanner.nextInt();
         Vector[] randVectArray = Vector.randomVectorArray(j);
         for (int i = 0; i < j; i++) {
@@ -34,8 +37,8 @@ public class Main {
         }
 
 
+        //Calculating dor product with random vector using overridden method
         System.out.println("Result of multiplication of the 1st vector " + vector1 + " with random vector is = " + vector1.multBy());
-
 
     }
 }
--- src/com/example/java/Vector.java
+++ src/com/example/java/Vector.java
@@ -10,13 +10,11 @@ public class Vector {
     }
 
     //Method to calculate length of the vector
-
     public double length() {
         return Math.sqrt(x * x + y * y + z * z);
     }
 
     //Method that calculates the dot product
-
     public double multBy(Vector B) {
         return this.x * B.x + this.y * B.y + this.z * B.z;
     }
@@ -31,23 +29,17 @@ public class Vector {
     }
 
     //Method that creates single random vector
-
     public static Vector randomVector() {
-
         return new Vector(Math.random(), Math.random(), Math.random());
-
     }
-
 
     //Static method that creates an array of random vectors
     public static Vector[] randomVectorArray(int j) {
-
         Vector[] vector = new Vector[j];
         for (int i = 0; i < j; i++) {
             vector[i] = Vector.randomVector();
         }
         return vector;
-
     }
 
     //Overloaded multiplication method to use random vector
